47d6621a4a4909c2c73b62681b357dd9cd1b1ec9
doas root <petr.zakharov.anton@gmail.com>
2025-02-13 13:05:23 +0300

    feat(): sqlalchemy infrastructure layer

---
+++ solution/app/core/infrastructure/models/client/sqlalchemy/client.py
@@ -0,0 +1,24 @@
+import enum
+import uuid
+
+from sqlalchemy import String, Integer, Enum
+from sqlalchemy.orm import Mapped, mapped_column
+
+from app.core.infrastructure.models.sqlalchemy import Base
+
+
+class Gender(enum.Enum):
+    MALE = "male"
+    FEMALE = "female"
+
+
+class Client(Base):
+    __tablename__ = "clients"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        default=uuid.uuid4, primary_key=True, unique=True
+    )
+    login: Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    age: Mapped[int] = mapped_column(Integer, nullable=False)
+    location: Mapped[str] = mapped_column(String, nullable=False)
+    gender: Mapped[Gender] = mapped_column(Enum(Gender), nullable=False)
---
+++ solution/app/core/infrastructure/models/sqlalchemy/__init__.py
@@ -0,0 +1,5 @@
+__all__ = ['Base']
+
+import sqlalchemy.ext.declarative
+
+Base: sqlalchemy.ext.declarative.DeclarativeMeta = sqlalchemy.ext.declarative.declarative_base()
